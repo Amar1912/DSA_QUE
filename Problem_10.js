d827587875b7308447506936b889c0bddfbee34f
Amar Dhadbale <163174930+Amar1912@users.noreply.github.com>
2026-07-17 16:07:50 +0530

    Fix comment formatting in Problem_10.js

--- Problem_10.js
+++ Problem_10.js
@@ -1,6 +1,6 @@
 /*
 Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
-Note that minor deviation from millis in the actual sleep duration is acceptable.
+Note that minor deviation from millis in the actual sleep duration is acceptable...
 */
 
 /**
